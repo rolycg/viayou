a3be6a89b10800d664372f38b61d83426a3ea5a2
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2016-01-30 00:34:28 -0500

    create service to handle multi language feature

---
+++ Source/Services/ViaYou.Services/Culture/ICultureService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViaYou.Services.Culture
+{
+    public interface ICultureService
+    {
+        string GetImplementedCulture(string name);
+    }
+}
