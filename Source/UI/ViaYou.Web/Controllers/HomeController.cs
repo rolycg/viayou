ece0a0c2227104b0a93271d4bf36342d9c37b81f
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2016-01-30 00:35:14 -0500

    create action methods for change language

--- Source/UI/ViaYou.Web/Controllers/HomeController.cs
+++ Source/UI/ViaYou.Web/Controllers/HomeController.cs
@@ -13,18 +13,21 @@ namespace ViaYou.Web.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult ChangeLanguage(string backUrl, string culture = "ES")
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            HttpCookie cookie = Request.Cookies["_cultureViaYou"];
+            if (cookie != null)
+                cookie.Value = culture;   // update cookie value
+            else
+            {
+                cookie = new HttpCookie("_cultureViaYou")
+                {
+                    Value = culture,
+                    Expires = DateTime.Now.AddYears(1)
+                };
+            }
+            Response.Cookies.Add(cookie);
+            return Redirect(backUrl);
         }
     }
 }
