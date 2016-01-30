362c9dcb6140b125c157639a4b7989e8dc6222dc
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2016-01-29 22:25:22 -0500

    create DashboardController

--- Source/UI/ViaYou.Web/Areas/Admin/AdminAreaRegistration.cs
+++ Source/UI/ViaYou.Web/Areas/Admin/AdminAreaRegistration.cs
@@ -11,7 +11,7 @@ namespace ViaYou.Web.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { controller="Users", action = "Index", id = UrlParameter.Optional }
+                new { controller="Dashboard", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
