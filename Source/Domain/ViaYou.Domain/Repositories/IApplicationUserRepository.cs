281f0aa9e26a02a4aca775f3caa4d05d4ef366b9
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2016-02-04 23:52:39 -0500

    create identity manager class and define application user repository

---
+++ Source/Domain/ViaYou.Domain/Repositories/IApplicationUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViaYou.Domain.Users;
+
+namespace ViaYou.Domain.Repositories
+{
+    public interface IApplicationUserRepository
+    {
+        ApplicationUser GetById(string id);
+        void Add(ApplicationUser user);
+        IQueryable<ApplicationUser> GetAll();
+        bool Add(ApplicationUser user, string password);
+    }
+}
