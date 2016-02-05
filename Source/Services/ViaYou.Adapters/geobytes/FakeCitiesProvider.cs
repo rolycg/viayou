72b5f0308352c58e2c6b8818bebefc5d4c89cac3
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2016-02-05 11:39:05 -0500

    create service mock for cities provider

---
+++ Source/Services/ViaYou.Adapters/geobytes/FakeCitiesProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViaYou.Adapters.geobytes.Models;
+
+namespace ViaYou.Adapters.geobytes
+{
+    public class FakeCitiesProvider : ICitiesProvider
+    {
+        public IEnumerable<GeobytesCity> GetCities(string term)
+        {
+            yield return new GeobytesCity("Madrid", 1);
+            yield return new GeobytesCity("Milano", 2);
+            yield return new GeobytesCity("Lisbon", 3);
+            yield return new GeobytesCity("Munich", 4);
+        }
+    }
+}
