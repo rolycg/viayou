4eba6bd5ef2080acfb32cac7f2f303c2e7ae9706
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2016-02-04 23:41:10 -0500

    create Travel and Message models and its repositories definition

---
+++ Source/Domain/ViaYou.Domain/Chat/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViaYou.Domain.Chat
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Source/Domain/ViaYou.Domain/Repositories/IMessageRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViaYou.Domain.Chat;
+
+namespace ViaYou.Domain.Repositories
+{
+    public interface IMessageRepository
+    {
+        void Add(Message message);
+        Message GetById(int id);
+        IQueryable<Message> GetAll();
+    }
+}
