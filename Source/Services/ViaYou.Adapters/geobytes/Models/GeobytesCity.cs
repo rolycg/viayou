60e98938b352e9f1ed483514ab666954be605c41
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2016-02-04 23:22:17 -0500

    create adapters project. define service for retrieve cities from the REST API

---
+++ Source/Services/ViaYou.Adapters/geobytes/Models/GeobytesCity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViaYou.Adapters.geobytes.Models
+{
+    public class GeobytesCity
+    {
+        public string Name { get; set; }
+        public int Code { get; set; }
+
+
+        public GeobytesCity()
+        {
+            
+        }
+        public GeobytesCity(string name, int code)
+        {
+            Name = name;
+            Code = code;
+        }
+    }
+}
--- Source/UI/ViaYou.Web/Controllers/HomeController.cs
+++ Source/UI/ViaYou.Web/Controllers/HomeController.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ViaYou.Adapters.geobytes;
 using ViaYou.Services.Culture;
 
 namespace ViaYou.Web.Controllers
 {
     public class HomeController : BaseController
     {
+        private ICitiesProvider _citiesProvider;
+
+        public HomeController(ICitiesProvider citiesProvider)
+        {
+            _citiesProvider = citiesProvider;
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -34,13 +42,8 @@ namespace ViaYou.Web.Controllers
         [AllowAnonymous]
         public JsonResult RetrieveCities(string searchTerm, int pageSize, int pageNum)
         {
-            var list = new List<string>
-            {
-                "La habana",
-                "Rio",
-                "Madrid"
-            };
-            var results = list.Where(x=>x.ToLower().Contains(searchTerm)).Select(c => new { id = c, text = c }).ToList();
+            var cities = _citiesProvider.GetCities(searchTerm);
+            var results = cities.Where(x=>x.Name.Contains(searchTerm) || x.Code.ToString().Contains(searchTerm)).Select(c => new { id = c.Code, text = c.Name }).ToList();
             return new JsonResult
             {
                 Data = new { Total = results.Count(), Results = results },
