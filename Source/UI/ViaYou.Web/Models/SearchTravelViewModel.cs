6fe1e4332cdc051ecefb879733925fe1c5b4cf16
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2016-02-09 22:38:28 -0500

    create SearchTravelViewModel class

---
+++ Source/UI/ViaYou.Web/Models/SearchTravelViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViaYou.Web.Models
+{
+    public class SearchTravelViewModel
+    {
+        public int From { get; set; }
+        public int To { get; set; }
+        public int CategoryId { get; set; }
+        public int ContainedInId { get; set; }
+    }
+}
