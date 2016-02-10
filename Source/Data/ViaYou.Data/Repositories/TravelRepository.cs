12b61e8fb3aff4eb746763c99a1ec40593ad3432
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2016-02-09 22:47:49 -0500

    create TravelRepository and register it in the UnityConfig file

---
+++ Source/Data/ViaYou.Data/Repositories/TravelRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViaYou.Domain.Repositories;
+using ViaYou.Domain.Travels;
+
+namespace ViaYou.Data.Repositories
+{
+    public class TravelRepository : ITravelRepository
+    {
+        public void Add(Travel travel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Travel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Travel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
