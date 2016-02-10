d261b00c288bbbd9e8a6eacb833cbccb345c82f7
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2016-02-09 23:20:33 -0500

    create action method for search travels

--- Source/UI/ViaYou.Web/Controllers/TravelsController.cs
+++ Source/UI/ViaYou.Web/Controllers/TravelsController.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ViaYou.Services.TravelSearchProvider;
+using ViaYou.Web.Models;
 
 namespace ViaYou.Web.Controllers
 {
     public class TravelsController : Controller
     {
+        private readonly ITravelSearchService _travelSearchService;
+
+        public TravelsController(ITravelSearchService travelSearchService)
+        {
+            _travelSearchService = travelSearchService;
+        }
+
         // GET: Travels
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult SearchTravels(SearchTravelViewModel data)
+        {
+            var travels = _travelSearchService.SearchTravels(data.From, data.To, data.CategoryId, data.ContainedInId);
+            return PartialView(travels);
         }
     }
 }
