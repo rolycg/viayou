56a362fa3110745f2bda67d97d95d8b94fbdf415
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2016-02-09 23:22:39 -0500

    add required validation to SearchTravelViewModel

--- Source/UI/ViaYou.Web/Models/SearchTravelViewModel.cs
+++ Source/UI/ViaYou.Web/Models/SearchTravelViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,13 @@ namespace ViaYou.Web.Models
 {
     public class SearchTravelViewModel
     {
+        [Required]
         public int From { get; set; }
+        [Required]
         public int To { get; set; }
+        [Required]
         public int CategoryId { get; set; }
+        [Required]
         public int ContainedInId { get; set; }
     }
 }
