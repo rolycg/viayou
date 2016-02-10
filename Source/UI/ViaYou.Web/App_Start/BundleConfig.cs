7b078430c9bbd1b74b6b5880ea30e6a56fcd5566
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2016-02-09 21:44:13 -0500

    fix scripts references issues for login and register modal

--- Source/UI/ViaYou.Web/App_Start/BundleConfig.cs
+++ Source/UI/ViaYou.Web/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace ViaYou.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                 "~/Scripts/jquery.validate*",
@@ -17,15 +17,17 @@ namespace ViaYou.Web
                 ));
 
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/login-register.js",
+                "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/Shared/css").Include(
                 "~/Content/Shared/css/bootstrap.min.css",
-                "~/Content/Shared/css/site.css"
+                "~/Content/Shared/css/site.css",
+                "~/Content/Shared/css/login-register.css"
                 ));
 
             bundles.Add(new StyleBundle("~/Content/BasicTemplate/css").Include(
